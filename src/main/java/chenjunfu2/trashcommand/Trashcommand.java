8f0591bb568a29006dac95e6d3793e94ef0abfe4
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2025-07-24 09:12:53 +0800

    清理无用语句

--- src/main/java/chenjunfu2/trashcommand/Trashcommand.java
+++ src/main/java/chenjunfu2/trashcommand/Trashcommand.java
@@ -7,7 +7,6 @@ import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.screen.GenericContainerScreenHandler;
 import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
-import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
